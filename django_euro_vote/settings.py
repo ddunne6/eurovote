48a88c9d1ae366797bc66e6ff3465054a3400309
David Dunne <50323487+ddunne6@users.noreply.github.com>
2022-05-13 13:20:46 +0100

    added azure site to allowed hosts

--- django_euro_vote/settings.py
+++ django_euro_vote/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'eurovote.azurewebsites.net']
 
 
 # Application definition
